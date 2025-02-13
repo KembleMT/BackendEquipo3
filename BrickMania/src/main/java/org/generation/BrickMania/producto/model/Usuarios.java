4284527f9a9b2243ed9f0ee4b4561faefdceb01e
aneth <aneth.mt@hotmail.com>
2025-02-13 12:39:52 -0600

    Se eliminan duplicados

--- BrickMania/src/main/java/org/generation/BrickMania/producto/model/Usuarios.java
+++ BrickMania/src/main/java/org/generation/BrickMania/producto/model/Usuarios.java
@@ -96,46 +96,6 @@ public class Usuarios {
     public boolean comparePassword(String rawPassword) {
         return new BCryptPasswordEncoder().matches(rawPassword, this.contraseña);
     }
-
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getContraseña() {
-		return contraseña;
-	}
-	public void setContraseña(String contraseña) {
-	    if (!contraseña.startsWith("$2a$")) { // Evita encriptar si ya está encriptado
-	        this.contraseña = new BCryptPasswordEncoder().encode(contraseña);
-	    } else {
-	        this.contraseña = contraseña;
-	    }
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	
-	public Integer getId_rol_fk() {
-	    return id_rol_fk;
-	}
-
-	public void setId_rol_fk(Integer id_rol_fk) {
-	    this.id_rol_fk = id_rol_fk;
-	}
-
-
 	@Override
 	public String toString() {
 		return "Usuarios [id=" + id + ", nombre=" + nombre + ", email=" + email + ", contraseña=" + contraseña
