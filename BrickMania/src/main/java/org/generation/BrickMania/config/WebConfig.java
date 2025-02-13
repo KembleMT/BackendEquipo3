c9c0716a3aaf67923cea39fa994cc05de0414073
Emmanuel Luna <emmanueleluna@gmail.com>
2025-02-12 23:08:17 -0600

    Implementación de la llamada del API desde el formulario de creación de productos

---
+++ BrickMania/src/main/java/org/generation/BrickMania/config/WebConfig.java
@@ -0,0 +1,23 @@
+package org.generation.BrickMania.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Permite las rutas que empiezan con /api/
+                        .allowedOrigins("http://127.0.0.1:5501") // Tu dirección de pruebas en VSC
+                        .allowedMethods("GET", "POST", "PUT", "DELETE"); // Métodos permitidos
+            }
+        };
+    }
+}
+
